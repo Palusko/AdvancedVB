b46abbc72a2290d035b6f21dcda38197a0e62213
Pavol <pavol@homerundesign.com>
2019-07-27 23:18:59 -0400

    001-BankTeller-Start

--- BankTellerApp/My Project/Application.Designer.vb
+++ BankTellerApp/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BankTellerApp.Form1
+            Me.MainForm = Global.BankTellerApp.frmBankTellerApp
         End Sub
     End Class
 End Namespace
